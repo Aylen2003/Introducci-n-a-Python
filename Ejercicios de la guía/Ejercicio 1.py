64f74cabd0a2746736f9d8cc1fa5b1fa3bc9e1c7
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-21 15:39:34 -0300

    Ejercicio 1.py

--- Ejercicios de la guía/Ejercicio 1.py
+++ Ejercicios de la guía/Ejercicio 1.py
@@ -1,1 +1,12 @@
+nota1 = float(input("Ingrese la 1er nota:"))
+nota2 = float(input("Ingrese la 2da nota: "))
+nota3 = float(input("Ingrese la 3er nota:"))
 
+promedio = (nota1 + nota2 + nota3) / 3
+
+if (promedio >= 7):
+    print("El alumno promociono la materia")
+elif(promedio >= 4):
+    print("El alumno debe rendir final")
+else:
+    print("El alumno debe recursar la materia")
