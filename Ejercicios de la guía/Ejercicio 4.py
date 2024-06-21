e77e5d0be226c1120c449009ff3f4349e68a66b3
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-21 15:45:05 -0300

    Create Ejercicio 4.py

---
+++ Ejercicios de la guía/Ejercicio 4.py
@@ -0,0 +1,5 @@
+base = int(input("Ingrese la  base del rectangulo:"))
+altura = int(input("Ingrese la altura del rectangulo:"))
+
+print("El perimetro del rectangulo es: {}".format(2*base + 2*altura))
+print("La superficie del rectangulo es: {}".format(base * altura))
