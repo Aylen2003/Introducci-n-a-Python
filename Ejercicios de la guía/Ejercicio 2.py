5fd4bcf8345a28231be951a122815e9dffebeb60
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-21 15:40:18 -0300

    Create Ejercicio 2.py

---
+++ Ejercicios de la guía/Ejercicio 2.py
@@ -0,0 +1,7 @@
+partidosGanados = int(input("Partidos ganados:"))
+partidosEmpatados = int(input("Partidos empatados:"))
+partidosPerdidos = int(input("Partidos perdidos:"))
+
+puntajeFinal = partidosGanados*3 + partidosEmpatados
+
+print(puntajeFinal)
